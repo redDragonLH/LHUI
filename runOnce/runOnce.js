65568344bb9fb67fe85ed6068815b11192e0e3b9
刘鹤 <1332983774@qq.com>
2016-09-22 09:15:39 +0800

    once完成

--- runOnce/runOnce.js
+++ runOnce/runOnce.js
@@ -1,13 +1,15 @@
-(runOnce = function (fn, context) { //控制让函数只触发一次
-  return function () {
-      try {
-          fn.apply(context || this, arguments);
-      }
-      catch (e) {
-          console.error(e);//一般可以注释掉这行
-      }
-      finally {
-          fn = null;
-      }
-  };
-})(window, document);
+(function(e){
+  runOnce= function(fn, context) { //控制让函数只触发一次
+    return function () {
+        try {
+            fn.apply(context || this, arguments);
+        }
+        catch (e) {
+            console.error(e);//一般可以注释掉这行
+        }
+        finally {
+            fn = null;
+        }
+    };
+  }
+})(window)
