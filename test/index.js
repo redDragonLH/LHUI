4a7da22abcc41dae762adbcb59bae404a3c64ff6
六合 <1332983774@qq.com>
2017-04-17 11:34:23 +0800

    ..

--- test/index.js
+++ test/index.js
@@ -6,6 +6,6 @@ tap.ok(index.getFile);
 tap.ok(index.getFilePath);
 
 // assertions
-tap.match(index.getFile('pure-min.css'), /pure\-button/, 'should load the file');
-tap.match(index.getFilePath('pure-min.css'), /pure\-min\.css/, 'should return file path');
+tap.match(index.getFile('lhui-min.css'), /lhui\-button/, 'should load the file');
+tap.match(index.getFilePath('lhui-min.css'), /lhui\-min\.css/, 'should return file path');
 tap.throws(index.getFile, new Error('undefined does not exist'));
